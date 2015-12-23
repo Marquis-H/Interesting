1fe24688f8ff8ce4df891025066627bc04fc02d8
marquis <511680713@qq.com>
2015-12-23 16:33:27 +0800

    update

--- wallpaper/py/wallpaper/wallpaper/settings.py
+++ wallpaper/py/wallpaper/wallpaper/settings.py
@@ -80,8 +80,8 @@ IMAGES_STORE = '/Users/marquis/Pictures/himawari8'
 
 # Enable and configure HTTP caching (disabled by default)
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED=True
-#HTTPCACHE_EXPIRATION_SECS=0
-#HTTPCACHE_DIR='httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES=[]
-#HTTPCACHE_STORAGE='scrapy.extensions.httpcache.FilesystemCacheStorage'
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache'
+HTTPCACHE_IGNORE_HTTP_CODES = []
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
